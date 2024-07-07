9e4e3a7062e0b4eda10df54b42af84e9415ac842
oralet59 <merocks86@gmail.com>
2024-07-07 11:57:11 +0300

    Implemented getting inputs and added checks for setting grid size

--- scripts.js
+++ scripts.js
@@ -1,9 +1,27 @@
 let grid_size = 16;
 
 const grid = document.querySelector("div.grid");
+const grid_input = document.querySelector("div.input input");
+const grid_span = document.querySelector("div.input span");
+const grid_button = document.querySelector("div.input button");
 
-for(let i=0;i<grid_size;i++) {
-    grid.appendChild(makeRow(grid_size));
+grid_button.addEventListener("click", () => {
+    let new_grid_size = parseInt(grid_input.value);
+    if(isNaN(new_grid_size)) {
+        grid_input.value = "";
+        alert("Please set the grid size to a valid value!");
+    }
+    else {
+        grid_size = new_grid_size;
+        setGrid(grid_size);
+    }
+});
+
+function setGrid(size) {
+    for(let i=0;i<size;i++) {
+        grid.appendChild(makeRow(size));
+    }
+    grid_span.innerText = `Set grid size (current size is ${size})`;
 }
 
 function makeRow(size) {
@@ -12,7 +30,7 @@ function makeRow(size) {
     row.style.display = "flex";
     row.style.flexDirection = "column";
     row.flex = "auto";
-    for(let i=0;i<grid_size;i++) {
+    for(let i=0;i<size;i++) {
         row.appendChild(makeGridTile());
     }
     return row;
@@ -22,6 +40,8 @@ function makeGridTile() {
     const grid_tile = document.createElement("div");
     grid_tile.className = "gridtile";
     grid_tile.style.flex = "auto";
-    grid_tile.style.border = "2px solid dimgray";
+    grid_tile.style.border = "1px solid dimgray";
     return grid_tile;
 }
+
+setGrid(grid_size);
